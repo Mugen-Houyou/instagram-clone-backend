3e5e56865e3a1c9537195610fa4ffe6e8ed544fb
Mugen-Houyou <a4r2@daum.net>
2022-03-15 19:44:24 -0400

    commit-2022-03-15-2

--- photos/toggleLike/toggleLike.resolvers.js
+++ photos/toggleLike/toggleLike.resolvers.js
@@ -5,7 +5,7 @@ import { processHashtags } from "../photos.utils";
 export default {
   Mutation: {
     toggleLike: protectedResolver(
-      async (_, { id, caption }, { loggedInUser }) => {
+      async (_, { id }, { loggedInUser }) => {
         const photo = await client.photo.findUnique({
           where: { id },
         });
@@ -18,18 +18,23 @@ export default {
           },
         };
 
+        // const like = await client.like.findUnique({
+        //   where: {
+        //     photoId_userId: {
+        //       userId: loggedInUser.id,
+        //       photoId: id,
+        //     }
+        //   },  // "이게 바로 이전 영상에서 말했던 것처럼 두 필드를 합쳐서 unique한 값을 만드는 것이야"
+        // });   //  @@unique([photoId, userId]) 이거 연결하는 거.
+
         const like = await client.like.findUnique({
-          where: {
-            photoId_userId: {
-              userId: loggedInUser.id,
-              photoId: id,
-            }
-          },  // "이게 바로 이전 영상에서 말했던 것처럼 두 필드를 합쳐서 unique한 값을 만드는 것이야"
-        });   //  @@unique([photoId, userId]) 이거 연결하는 거.
+          where: likeWhere
+        });
+
         if (like) {
           await client.like.delete({
             where: likeWhere, // 이렇게 중복되는 부분을 대체할 수도 있음
-          });
+          }); ``
         } else {
           await client.like.create({
             data: {
