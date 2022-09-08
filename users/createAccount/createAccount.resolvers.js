a960c1840ebf3b4232dc742c0fbbbef7ef1352ed
Mugen-Houyou <a4r2@daum.net>
2022-09-07 22:46:58 -0400

    09-07-2022, me Query and more...

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -18,8 +18,11 @@ export default {
 
         if (existingUser) throw new Error("This username/password is already taken.");
 
-        const uglyPassword = await bcrypt.hash(password, 10);
-        return client.user.create({
+        console.log("Generating the password hash...")
+        // 주의: Increasing the cost factor by 1 doubles the necessary time. 
+        const uglyPassword = await bcrypt.hash(password, 16); 
+        console.log("Password hash done!")
+        await client.user.create({
           data: {
             username,
             email,
@@ -28,6 +31,18 @@ export default {
             password: uglyPassword,
           },
         });
+        // return client.user.create({
+        //   data: {
+        //     username,
+        //     email,
+        //     firstName,
+        //     lastName,
+        //     password: uglyPassword,
+        //   },
+        // });
+        return {
+          ok: true,
+        };
       } catch (e) {
         return e;
       }
